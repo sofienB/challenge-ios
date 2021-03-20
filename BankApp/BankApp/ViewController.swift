bc9ba666e927236f016b3d6d35885ae3221b62d5
sofien Benharchache <sofien.benharchache@adotmob.com>
2021-03-20 20:36:21 +0100

    [BA-001] Fist commit with Xcode project starter

---
+++ BankApp/BankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by sofien Benharchache on 20/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
